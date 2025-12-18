4a61a5804e6b17451e1225749aeff6a04b0bdcb3
Simon Wilson <ksattic@gmail.com>
2025-12-17 17:23:25 -0700

    Day 8: code tidy up

--- day08/src/Day08.kt
+++ day08/src/Day08.kt
@@ -5,8 +5,6 @@ class Day08(input: String) {
         val parts = it.split(",")
         Point3D(parts[0].toInt(), parts[1].toInt(), parts[2].toInt())
     }
-    private val circuits = mutableListOf<MutableSet<Point3D>>()
-    private val pointToCircuit = mutableMapOf<Point3D, Int>()
     private val distances = findDistances()
 
     data class Point3DDistances(val a: Point3D, val b: Point3D, val distance: Double)
@@ -24,35 +22,11 @@ class Day08(input: String) {
         return distances.sortedBy { it.distance }
     }
 
-    init {
-        for (j in junction) {
-            circuits.add(mutableSetOf(j))
-            pointToCircuit[j] = circuits.lastIndex
-        }
-    }
-
-    /*private fun findShortestPair(): Pair<Point3D, Point3D>? {
-        var shortest = Double.MAX_VALUE
-        var pair: Pair<Point3D, Point3D>? = null
-        for (i in junction.indices) {
-            for (j in i + 1 until junction.size) {
-                val aCircuit = pointToCircuit[junction[i]]!!
-                val bCircuit = pointToCircuit[junction[j]]!!
-                // skip if they are already in the same circuit
-                if (aCircuit == bCircuit) {
-                    continue
-                }
-                val dist = junction[i].euclideanSquare(junction[j])
-                if (dist < shortest) {
-                    shortest = dist
-                    pair = Pair(junction[i], junction[j])
-                }
-            }
-        }
-        return pair
-    }*/
-
-    private fun mergeCircuits(aCircuit: Int, bCircuit: Int) {
+    private fun mergeCircuits(
+        circuits: MutableList<MutableSet<Point3D>>,
+        pointToCircuit: MutableMap<Point3D, Int>,
+        aCircuit: Int, bCircuit: Int
+    ) {
         val bJunctions = circuits[bCircuit]
         for (j in bJunctions) {
             pointToCircuit[j] = aCircuit
@@ -62,6 +36,13 @@ class Day08(input: String) {
     }
 
     fun part1(): Int {
+        val circuits = mutableListOf<MutableSet<Point3D>>()
+        val pointToCircuit = mutableMapOf<Point3D, Int>()
+        for (j in junction) {
+            circuits.add(mutableSetOf(j))
+            pointToCircuit[j] = circuits.lastIndex
+        }
+
         repeat(1000) { index ->
             val pair = distances[index]
 
@@ -69,7 +50,7 @@ class Day08(input: String) {
             val bCircuit = pointToCircuit[pair.b]!!
 
             if (aCircuit != bCircuit) {
-                mergeCircuits(aCircuit, bCircuit)
+                mergeCircuits(circuits, pointToCircuit, aCircuit, bCircuit)
             }
         }
 
@@ -79,6 +60,12 @@ class Day08(input: String) {
     }
 
     fun part2(): Long {
+        val circuits = mutableListOf<MutableSet<Point3D>>()
+        val pointToCircuit = mutableMapOf<Point3D, Int>()
+        for (j in junction) {
+            circuits.add(mutableSetOf(j))
+            pointToCircuit[j] = circuits.lastIndex
+        }
         var circuitCount = circuits.size
         var index = 0
 
@@ -90,7 +77,7 @@ class Day08(input: String) {
             val bCircuit = pointToCircuit[pair.b]!!
 
             if (aCircuit != bCircuit) {
-                mergeCircuits(aCircuit, bCircuit)
+                mergeCircuits(circuits, pointToCircuit, aCircuit, bCircuit)
                 circuitCount--
                 if (circuitCount == 1) {
                     // last pair just merged
@@ -105,6 +92,6 @@ class Day08(input: String) {
 
 fun main() {
     val aoc = Day08("day08/input.txt")
-    //println(aoc.part1())
+    println(aoc.part1())
     println(aoc.part2())
 }
